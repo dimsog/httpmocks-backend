0cb935088f4a52076826e44f4c44a4aada86b3a0
Dmitry R <dimsog@gmail.com>
2025-12-15 21:51:50 +0300

    wip: render

---
+++ internal/http-server/render/render.go
@@ -0,0 +1,29 @@
+package render
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Error struct {
+	Success bool              `json:"success"`
+	Text    string            `json:"text"`
+	Data    map[string]string `json:"data"`
+}
+
+func ValidationError(w http.ResponseWriter, err error) error {
+	validationError := newValidationError(err)
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	return Json(w, validationError)
+}
+
+func Json(w http.ResponseWriter, v any) error {
+	return json.NewEncoder(w).Encode(v)
+}
+
+func newValidationError(err error) Error {
+	return Error{
+		Success: false,
+		Text:    "Validation error",
+	}
+}
